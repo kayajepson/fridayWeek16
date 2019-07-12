8ceae2603db3dab05a276f0d574e1fbd63a0e8f6
kaya <jepsonkaya@gmail.com>
2019-07-12 10:34:20 -0700

    trying to get props to pass data

--- src/App.js
+++ src/App.js
@@ -18,15 +18,51 @@ class App extends React.Component {
       availibility: "",
       quote: "",
       rating: "",
-      color: "",
-
     };
     this.handleCurrentKombucha = this.handleCurrentKombucha.bind(this);
+    this.handleKombuchaName = this.handleKombuchaName.bind(this);
+    this.handleDescription = this.handleDescription.bind(this);
+    this.handleAvailibility = this.handleAvailibility.bind(this);
+    this.handleQuote = this.handleQuote.bind(this);
+    this.handleRating = this.handleRating.bind(this);
   }
+
+  handleKombuchaName(){
+    var kombuchaName = this.state.name;
+  }
+
+  handleDescription(){
+    var kombuchaDescription = this.state.description;
+  }
+
+  handleAvailibility(){
+    var kombuchaAvailibility = this.state.availibility;
+  }
+
+  handleQuote(){
+    var kombuchaQuote = this.state.quote;
+  }
+
+  handleRating(){
+    var kombuchaRating = this.state.rating;
+  }
+
 
   handleCurrentKombucha = ev => {
     this.setState({
-      currentKombucha: ev.currentTarget.dataset.div_id
+      divId = ev.currentTarget.dataset.div_id,
+      (divId === "1") ? this.currentKombucha: "Clear Mind":
+      (divId === "2") ? this.currentKombucha: "Love":
+      (divId === "3") ? this.currentKombucha: "Mint Lemonade":
+      (divId === "4") ? this.currentKombucha: "Ginger Tumeric":
+      (divId === "5") ? this.currentKombucha: "Strawberry Basil":
+      (divId === "6") ? this.currentKombucha: "Power Flower":
+      (divId === "7") ? this.currentKombucha: "Superberry":
+      (divId === "8") ? this.currentKombucha: "Uplift":
+      (divId === "9") ? this.currentKombucha: "Happiness":
+      (divId === "10") ? this.currentKombucha: "Spiced Apple":
+      (divId === "11") ? this.currentKombucha: "Citrus Hops":
+      (divId === "12") ? this.currentKombucha: "Lemon Ginger Cayenne": this.currentKombucha: "Null"
     });
   };
 
@@ -38,7 +74,12 @@ class App extends React.Component {
       <Route exact path='/' component={MainContainer} />
       <Route path='/clearmind' component={ClearMind} />
       <Route path='/aboutUs' component={AboutUs} />
-      <Route path='/kombuchaDetails'render={()=><KombuchaDetails onHandleCurrentKombucha={this.handleCurrentKombucha}/>}/>
+      <Route path='/kombuchaDetails'render={()=><KombuchaDetails onHandleCurrentKombucha={this.handleCurrentKombucha}
+      onKombuchaName={this.handleKombuchaName}
+      onHandleDescription={this.handleDescription}
+      onHandleAvailibility={this.handleAvailibility}
+      onHandleQuote={this.handleQuote}
+      onHandleRating={this.handleRating}/>}/>
       </Switch>
       </BrowserRouter>
       </div>
--- src/KombuchaDetails.jsx
+++ src/KombuchaDetails.jsx
@@ -7,188 +7,179 @@ import Details from './Details';
 import Kombucha from './Kombucha';
 import { withRouter } from 'react-router-dom';
 
-class KombuchaDetails extends React.Component{
-  constructor(props) {
-    super(props);
-    // this.handleVoteSubmission = this.handleVoteSubmission.bind(this);
+function KombuchaDetails (props){
+console.log(props);
+  var myKombuchaDetailsStyles = {
+    backgroundColor: '#fafafa',
+    display: 'grid',
+    gridTemplateColumns: '100vr',
   }
 
-  handleVoteSubmission(event) {
-    event.preventDefault();
+  var contentStyles = {
+    // marginTop: '10%',
+    backgroundImage: "url(" + Background + ")",
+    backgroundSize: 'cover',
+    minHeight: '95vh',
+    backgroudPosition: 'fixed',
+    backgroundRepeat: 'no repeat',
   }
 
+  var headerImgStyle = {
+    marginTop: '10%',
+    backgroundSize: 'cover',
+    width: '75%',
+    backgroudPosition: 'fixed',
+    backgroundRepeat: 'no repeat',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+  }
 
-  render() {
-    var myKombuchaDetailsStyles = {
-      backgroundColor: '#fafafa',
-      display: 'grid',
-      gridTemplateColumns: '100vr',
-    }
+  var rectangleStyle = {
+    marginTop: '3%',
+    backgroundColor: '#7ca7ad',
+    width: '75%',
+    height: '550px',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    zIndex: '5',
+  }
 
-    var contentStyles = {
-      // marginTop: '10%',
-      backgroundImage: "url(" + Background + ")",
-      backgroundSize: 'cover',
-      minHeight: '95vh',
-      backgroudPosition: 'fixed',
-      backgroundRepeat: 'no repeat',
-    }
+  var nameStyle = {
+    color: "#ffffff",
+    fontSize: '75px',
+    position: 'sticky',
+    lineHeight: '45px',
+    fontWeight: '600',
+    textAlign: 'center',
+    fontFamily: 'Oswald, sans-serif',
+    fontStretch: 'ultra-condensed',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginTop: '-37px',
+    marginRight: 'auto',
+    zIndex: '10',
+  }
 
-    var headerImgStyle = {
-      marginTop: '10%',
-      backgroundSize: 'cover',
-      width: '75%',
-      backgroudPosition: 'fixed',
-      backgroundRepeat: 'no repeat',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-    }
+  var descriptionStyle = {
+    // marginTop: '-5%',
+    color: "#ffffff",
+    fontSize: '25px',
+    fontWeight: '300',
+    textAlign: 'center',
+    fontFamily: 'Josefin Sans, sans-serif',
+    fontStretch: 'ultra-condensed',
+    fontStyle: 'italic',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    zIndex: '10',
+  }
 
-    var rectangleStyle = {
-      marginTop: '3%',
-      backgroundColor: '#7ca7ad',
-      width: '75%',
-      height: '550px',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      zIndex: '5',
-    }
+  var availibilityStyle = {
+    // marginTop: '-5%',
+    color: "#ffffff",
+    fontSize: '20px',
+    fontWeight: '300',
+    textAlign: 'center',
+    fontFamily: 'Josefin Sans, sans-serif',
+    fontStretch: 'ultra-condensed',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    zIndex: '10',
+  }
 
-    var nameStyle = {
-      color: "#ffffff",
-      fontSize: '75px',
-      position: 'sticky',
-      lineHeight: '45px',
-      fontWeight: '600',
-      textAlign: 'center',
-      fontFamily: 'Oswald, sans-serif',
-      fontStretch: 'ultra-condensed',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginTop: '-37px',
-      marginRight: 'auto',
-      zIndex: '10',
-    }
+  var quoteStyle = {
+    // marginTop: '-5%',
+    color: "#ffffff",
+    fontSize: '25px',
+    fontWeight: '600',
+    textAlign: 'center',
+    fontFamily: 'Josefin Sans, sans-serif',
+    fontStretch: 'ultra-expanded',
+    fontStyle: 'italic',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    zIndex: '10',
+  }
 
-    var descriptionStyle = {
-      // marginTop: '-5%',
-      color: "#ffffff",
-      fontSize: '25px',
-      fontWeight: '300',
-      textAlign: 'center',
-      fontFamily: 'Josefin Sans, sans-serif',
-      fontStretch: 'ultra-condensed',
-      fontStyle: 'italic',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      zIndex: '10',
-    }
+  var ratingStyle = {
+    // marginTop: '-5%',
+    color: "#ffffff",
+    fontSize: '15px',
+    fontWeight: '600',
+    textAlign: 'center',
+    fontFamily: 'Oswald, sans-serif',
+    fontStretch: 'ultra-condensed',
+    backgroudPosition: 'absolute',
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    zIndex: '10',
+  }
 
-    var availibilityStyle = {
-      // marginTop: '-5%',
-      color: "#ffffff",
-      fontSize: '20px',
-      fontWeight: '300',
-      textAlign: 'center',
-      fontFamily: 'Josefin Sans, sans-serif',
-      fontStretch: 'ultra-condensed',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      zIndex: '10',
-    }
-
-    var quoteStyle = {
-      // marginTop: '-5%',
-      color: "#ffffff",
-      fontSize: '25px',
-      fontWeight: '600',
-      textAlign: 'center',
-      fontFamily: 'Josefin Sans, sans-serif',
-      fontStretch: 'ultra-expanded',
-      fontStyle: 'italic',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      zIndex: '10',
-    }
-
-    var ratingStyle = {
-      // marginTop: '-5%',
-      color: "#ffffff",
-      fontSize: '15px',
-      fontWeight: '600',
-      textAlign: 'center',
-      fontFamily: 'Oswald, sans-serif',
-      fontStretch: 'ultra-condensed',
-      backgroudPosition: 'absolute',
-      display: 'block',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      zIndex: '10',
-    }
-    return (
-      <div style={myKombuchaDetailsStyles}>
-      <NavBar/>
-      <div class="divider"></div>
-      <div style={contentStyles}>
-      <img class="activator" alt="logo" style={headerImgStyle} src={require('./assets/CM-Desktop.png')}/>
-      {/*<Kombucha/>*/}
-      <div style={rectangleStyle}>
-      <h2 style={nameStyle}>{props.name}CLEAR MIND</h2>
-      <br/><br/>
-      <span style={descriptionStyle}>{props.description}A fan favorite, this brilliant blend of rosemary, mint, sage and green tea is <br/> bright and invigorating.</span>
-      <br/><br/>
-      <span style={availibilityStyle}>{props.availibility}Available in: 14oz bottles, 32oz bottles, 12oz cans & kegs.</span>
-      <br/><br/>
-      <span style={quoteStyle}>{props.quote}"My favorite kombucha flavor out there!"</span>
-      <br/><br/>
-      <span style={ratingStyle}>
-      {/* if (parseFloat({props.rating}) < 5) {
+  return (
+    <div style={myKombuchaDetailsStyles}>
+    <NavBar/>
+    <div class="divider"></div>
+    <div style={contentStyles}>
+    <img class="activator" alt="logo" style={headerImgStyle} src={require('./assets/CM-Desktop.png')}/>
+    {/*<Kombucha/>*/}
+    <div style={rectangleStyle}>
+    <h2 style={nameStyle}>{props.name}</h2>
+    <br/><br/>
+    <span style={descriptionStyle}>{props.description}A fan favorite, this brilliant blend of rosemary, mint, sage and green tea is <br/> bright and invigorating.</span>
+    <br/><br/>
+    <span style={availibilityStyle}>{props.availibility}Available in: 14oz bottles, 32oz bottles, 12oz cans & kegs.</span>
+    <br/><br/>
+    <span style={quoteStyle}>{props.quote}"My favorite kombucha flavor out there!"</span>
+    <br/><br/>
+    <span style={ratingStyle}>
+    {/* if (parseFloat({props.rating}) < 5) {
+      return (
+        <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+        <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+        <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+        <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+        <img alt="star" src={require('./assets/white-star-75-filled.png')}/><h6>{props.rating}</h6>
+      ) else (
         return (
           <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
           <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
           <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
           <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-          <img alt="star" src={require('./assets/white-star-75-filled.png')}/><h6>{props.rating}</h6>
-        ) else (
-          return (
-            <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-            <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-            <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-            <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-            <img alt="star" src={require('./assets/white-star-100-filled.png')}/><h6>{props.rating}</h6>
-          )
+          <img alt="star" src={require('./assets/white-star-100-filled.png')}/><h6>{props.rating}</h6>
         )
-      };*/}
-      <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-      <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-      <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-      <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
-      <img alt="star" src={require('./assets/white-star-75-filled.png')}/><h6>4.7</h6>
-      </span>
-      </div>
-      </div>
-      </div>
-    )
-  }
+      )
+    };*/}
+    <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+    <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+    <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+    <img alt="star" src={require('./assets/white-star-100-filled.png')}/>
+    <img alt="star" src={require('./assets/white-star-75-filled.png')}/><h6>4.7</h6>
+    </span>
+    </div>
+    </div>
+    </div>
+  )
 }
 
 KombuchaDetails.propTypes = {
-    name: PropTypes.string,
-    description: PropTypes.string,
-    availibility: PropTypes.string,
-    quote: PropTypes.string,
-    rating: PropTypes.string,
-    color: PropTypes.string,
+  currentKombucha: PropTypes.string,
+  name: PropTypes.string,
+  description: PropTypes.string,
+  availibility: PropTypes.string,
+  quote: PropTypes.string,
+  rating: PropTypes.string,
+  color: PropTypes.string,
 };
 
 export default KombuchaDetails;
